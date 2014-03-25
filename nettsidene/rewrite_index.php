e13e725b08091ded778fa6f3ea85803f19330e5e
Henrik Steen <henrist@henrist.net>
2014-03-25 14:17:43 +0100

    La inn en fil som setter inn innhold i index-fila med referanse til arkivet.

---
+++ nettsidene/rewrite_index.php
@@ -0,0 +1,23 @@
+<?php
+
+header("Content-Type: text/html; charset=windows-1252");
+$data = file_get_contents("statutter/index.html");
+
+
+// vi setter inn innehold etter første avsnitt
+
+$offset = 0;
+for ($i = 0; $i < 2; $i++)
+{
+	$offset = strpos($data, "<p", $offset+1);
+}
+
+echo substr($data, 0, $offset);
+
+echo '<p style="text-align: center; font-size: 110%; font-style: italic">For arkiv over tidligere dokumenter: <a href="http://blindern-studenterhjem.no/dokumenter/statutter_arkiv/">http://blindern-studenterhjem.no/dokumenter/statutter_arkiv/</a></p>';
+
+echo substr($data, $offset);
+
+die;
+
+echo $data;
